73411943278fed8c4ded3beaccf4a32bfbab3ba3
Eric Brohman <ericbrohman@gmail.com>
2014-12-16 12:37:56 -0600

    no fixtures

--- js/app.js
+++ js/app.js
@@ -1,64 +1,64 @@
 App = Ember.Application.create();
 
 App.Router.map(function() {
-	this.resource('person');
-	this.resource('workplace');
+	this.resource('people');
+	this.resource('person', {path: '/people/:id'});
 });
 
-App.ApplicationAdapter = DS.FixtureAdapter;
 
-App.Person = DS.Model.extend({
-	userName: DS.attr('string'),
-	role: DS.attr('string')
-});
-
-App.PersonController = Ember.ArrayController.extend({
-
-});
-
-App.Person.reopenClass({
-	FIXTURES: [
+var people = [
 		{
 			id: 1,
 			userName: 'Demo1',
-			role: 'admin'
+			role: 'admin',
+			email: 'demo1@example.com'
 		},
 		{
 			id: 2,
 			userName: 'Demo2',
-			role: 'adminAssistant'
+			role: 'adminAssistant',
+			email: 'demo1@example.com'
 		},
 		{
 			id: 3,
 			userName: 'Demo3',
-			role: 'deptHead'
+			role: 'deptHead',
+			email: 'demo1@example.com'
 		},
 		{
 			id: 4,
 			userName: 'Demo4',
 			role: 'groupTeam',
+			email: 'demo1@example.com'
 		},
 		{
 			id: 5,
 			userName: 'Demo5',
-			role: 'principal'
+			role: 'principal',
+			email: 'demo1@example.com'
 		},
 		{
 			id: 6,
 			userName: 'Demo6',
-			role: 'teacher'
+			role: 'teacher',
+			email: 'demo1@example.com'
 		}
-	]
-});
+	];
 
 
 
-App.PersonRoute = Ember.Route.extend({
-  model: function(role) {
-    return this.store.find('person');
+
+App.PeopleRoute = Ember.Route.extend({
+  model: function() {
+    return people;
   }
 });
 
+// App.PersonRoute = Ember.Route.extend({
+// 	model: function(){
+// 		return 
+// 	}
+// });
 
 
 window.EMBER_INSPECTOR_CONFIG = null;
